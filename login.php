b298c1405344c2e2b6e141f89da7a18e7ccf3eac
alexacost <79877564+alexacost@users.noreply.github.com>
2021-12-09 23:35:16 -0300

    arreglos php y css

--- login.php
+++ login.php
@@ -6,6 +6,7 @@
     header('Location: /dreamteam_final');
   }
   require 'database.php';
+  error_reporting(0);
 
   if (!empty($_POST['email']) && !empty($_POST['password'])) {
     $records = $conn->prepare('SELECT user,id, email, password FROM usuarios WHERE email = :email');
@@ -15,7 +16,7 @@
 
     $message = '';
 
-    if (count($results) > 0 && password_verify($_POST['password'], $results['password'])) {
+    if (password_verify($_POST['password'], $results['password'])) {
       $_SESSION['user_id'] = $results['id'];
       header("Location: /dreamteam_final");
     } else {
@@ -29,9 +30,9 @@
 <html>
   <head>
     <meta charset="utf-8">
-    <title>Login hola</title>
+    <title>Login</title>
     <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
-    <link rel="stylesheet" href="css/estilos.css">
+    <link rel="stylesheet" href="styles.css">
   </head>
   <body>
 
